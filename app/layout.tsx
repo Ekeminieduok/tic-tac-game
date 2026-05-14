2f6f8bdddc860bb0f9e380885aa7d51674d9de41
ekemini <rickyeduok@gmail.com>
2026-05-14 16:25:06 +0100

    addition: cover image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import { ReactNode } from "react";
 
-export const metadata: Metadata = {
-  title: "Tic·Tac·Toe",
-  description: "A classic game, refactored with Next.js",
+export const metadata = {
+  title: "crossly",
+  description: "A short description of your site",
+  openGraph: {
+    title: "Crossly",
+    description: "A short description of your site",
+    url: "https://crossly.netlify.app/",
+    images: [
+      {
+        url: "https://crossly.netlify.app/coverimage.png",
+        width: 1200,
+        height: 630,
+        alt: "Your Site Name",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://crossly.netlify.app/coverimage.png"],
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
